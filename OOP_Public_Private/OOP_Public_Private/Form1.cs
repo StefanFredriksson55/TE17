348282131632462a7f7846e96b32f871ac907860
stefanfredriksson55 <stefan.fredriksson@gmail.com>
2019-09-17 14:24:58 +0200

    2019-09-17

---
+++ OOP_Public_Private/OOP_Public_Private/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Public_Private
+{
+    public partial class Form1 : Form
+    {
+        Student[] studentlista = new Student[100];
+        int antalStudenter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student s1 = new Student("Stefan");
+            studentlista[antalStudenter++] = s1;
+
+
+            Student s2 = new Student("Jenny");            
+            //s1.printInfo();
+            MessageBox.Show(s1.getNamn());
+            MessageBox.Show(s1.Namn);
+            s1.setNamn("Hugo");
+            s1.Namn = "Hugo";
+            //s1.printInfo();
+        }
+    }
+}
---
+++ OOP_Public_Private/OOP_Public_Private/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Public_Private
+{
+    class Student
+    {
+        //Variabler
+        private string namn;
+        public Student(string n)
+        {
+            this.namn = n;
+        }
+
+        public void printInfo()
+        {
+            MessageBox.Show("Eleven heter" + this.namn);
+        }
+
+        public string getNamn()
+        {
+            return this.namn;
+        }
+        public void setNamn(string n)
+        {
+            if (n.Length > 0)
+            {
+                this.namn = n;
+            }            
+        }
+
+        public string Namn
+        {
+            get
+            {
+                return this.namn;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.namn = value;
+                }
+                
+            }
+        }
+    }
+}
