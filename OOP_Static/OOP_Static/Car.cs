6c65b9a8700b852d0bf53031d69ef08ced33ef67
stefanfredriksson55 <stefan.fredriksson@gmail.com>
2019-09-24 14:49:50 +0200

    2019-09-24

---
+++ OOP_Static/OOP_Static/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Static
+{
+    class Car
+    {
+        public static List<Car> carList = new List<Car>();
+        public static int antalBilar;
+
+        string _brand;
+        public Car(string brand)
+        {
+            this._brand = brand;
+        }
+
+        public static void minMetod()
+        {
+            //
+        }
+    }
+}
---
+++ OOP_Static/OOP_Static/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Static
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Car myCar = new Car("Volvo");
+            Car.carList.Add(myCar);            
+            Car.carList.Add(new Car("Saab"));
+
+            Car.minMetod();
+
+            Random myRandom = new Random();
+            int a = myRandom.Next(1, 7);
+
+
+            double pi = Math.PI;
+
+        }
+    }
+}
