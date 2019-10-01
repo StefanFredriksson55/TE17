18488d3f71d997911be901b9a0baf6898b82213c
stefanfredriksson55 <stefan.fredriksson@gmail.com>
2019-10-01 14:14:07 +0200

    2019-10-01

---
+++ OOP_foreach/OOP_foreach/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_foreach
+{
+    class Car
+    {
+        string _regNr;
+        public Car (string regnr)
+        {
+            this._regNr = regnr;
+        }
+
+        public override string ToString()
+        {
+            return this._regNr;
+        }
+    }
+}
---
+++ OOP_foreach/OOP_foreach/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_foreach
+{
+    public partial class Form1 : Form
+    {
+        List<Car> carList = new List<Car>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carList.Add(new Car("ABC123"));
+            carList.Add(new Car("DEF456"));
+
+            for (int i = 0; i < carList.Count; i++)
+            {
+                MessageBox.Show(carList[i].ToString());
+            }
+
+            foreach(Car c in carList)
+            {
+                MessageBox.Show(c.ToString());
+            }
+
+        }
+    }
+}
